6f3b5c458f0ee0bb7c8e09530c6d4c297b9693e2
JimVanG <jjvg@me.com>
2014-02-24 18:33:57 -0500

    Added list fragment and fetch class

--- src/com/sigmobile/ucf_news/FeedActivity.java
+++ src/com/sigmobile/ucf_news/FeedActivity.java
@@ -1,22 +1,12 @@
 package com.sigmobile.ucf_news;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
 
-public class FeedActivity extends Activity {
+public class FeedActivity extends SingleFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_feed);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.feed, menu);
-		return true;
+	protected Fragment createFragment() {
+		return new FeedFragment();
 	}
 
 }
