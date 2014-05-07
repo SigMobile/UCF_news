2637c754be45cab78509b83cb1b0d67e3d6607c6
JimVanG <jjvg@me.com>
2014-05-07 13:48:08 -0400

    Home is now the main activity

--- src/com/sigmobile/ucf_news/HomeActivity.java
+++ src/com/sigmobile/ucf_news/HomeActivity.java
@@ -1,9 +1,69 @@
 package com.sigmobile.ucf_news;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class HomeActivity extends Activity {
 
-	
-	
+	private ImageButton mImageButtonNews, mImageButtonText, mImageButtonMap,
+			mImageButtonEvents, mImageButtonSports;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+
+		mImageButtonNews = (ImageButton) findViewById(R.id.home_imageButton_one);
+		mImageButtonNews.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(),
+						FeedActivity.class);
+				startActivity(i);
+			}
+		});
+
+		mImageButtonText = (ImageButton) findViewById(R.id.home_imageButton_two);
+		mImageButtonText.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		mImageButtonEvents = (ImageButton) findViewById(R.id.home_imageButton_three);
+		mImageButtonEvents.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		mImageButtonMap = (ImageButton) findViewById(R.id.home_imageButton_four);
+		mImageButtonMap.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		mImageButtonSports = (ImageButton) findViewById(R.id.home_imageButton_five);
+		mImageButtonSports.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+	}
+
 }
