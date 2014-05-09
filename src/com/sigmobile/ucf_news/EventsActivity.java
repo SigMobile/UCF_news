1cf25a1a5f71e97751cee8cbe662ab85bf66416a
JimVanG <jjvg@me.com>
2014-05-08 20:13:28 -0400

    Added emtpy events activity.

---
+++ src/com/sigmobile/ucf_news/EventsActivity.java
@@ -0,0 +1,14 @@
+package com.sigmobile.ucf_news;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventsActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
